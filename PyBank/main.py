7dc5b27dfa15e55704798eb483b8d2c70d2b0bfc
johnshoyj <145418947+johnshoyj@users.noreply.github.com>
2023-10-22 23:07:44 -0500

    Comments commit

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,15 +1,22 @@
 import os
 import csv
 
+#bring in file
 budget_data = os.path.join("Resources", "budget_data.csv")
 
+#save output as list
+output = []
+
+#read the csv file
 with open(os.path.abspath(budget_data)) as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
 
+    #saves csv data to lists, create skip header row variable
     pl = [] 
     dates = []
     skip_head = 1
 
+    #loop through data and save to lists
     for row in csvreader:
 
         if skip_head:
@@ -18,15 +25,19 @@ with open(os.path.abspath(budget_data)) as csvfile:
             pl.append(int(row[1]))
             dates.append(row[0])
 
+    #count rows
     row_count = len(pl)
 
+    #creating counters
     sum = 0
     prev_row = 0
     total_change = 0
     skip_head = 1
 
+    #list to hold changes
     changes = []
 
+    #calculates change between rows
     for row in pl:
         sum += row
         if skip_head:
@@ -36,12 +47,14 @@ with open(os.path.abspath(budget_data)) as csvfile:
             changes.append(row-prev_row)
         prev_row = row
 
+    #create tracking variables for max increase and decrease
     max_increase = 0
     max_increase_index = 0
     max_decrease = 0
     max_decrease_index = 0
     index = 0
 
+    #find max increase and decrease in the changes list 
     for row in changes:
          if row > max_increase:
             max_increase = row
@@ -53,12 +66,20 @@ with open(os.path.abspath(budget_data)) as csvfile:
         
          index += 1
 
+    #formats the date below
     increase_parts = dates[max_increase_index].split("-")
     decrease_parts = dates[max_decrease_index].split("-")
 
-    print(row_count)
-    print(f'${sum}')
-    print(f'${total_change/(row_count-1):.2f}')
-    print(f'Greatest Increase in Profits:{increase_parts[1]}-{increase_parts[0]} $({max_increase})')
-    print(f'Greatest Decrease in Profits:{decrease_parts[1]}-{decrease_parts[0]} $({max_decrease})')
+    #print statements
+    output.append(row_count)
+    output.append(f'${sum}')
+    output.append(f'${total_change/(row_count-1):.2f}')
+    output.append(f'Greatest Increase in Profits:{increase_parts[1]}-{increase_parts[0]} $({max_increase})')
+    output.append(f'Greatest Decrease in Profits:{decrease_parts[1]}-{decrease_parts[0]} $({max_decrease})')
     
+    #output to a text file
+    with open ("output.txt","w") as f:
+        f.writelines(output)
+    #output to terminal
+    for line in output: 
+        print(line)
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,16 +1,23 @@
 import os
 import csv
 
+#bring in csv file
 election_data = os.path.join("Resources", "election_data.csv")
 
+#create list of output statements
+output = []
+
+#read csv file
 with open(os.path.abspath(election_data)) as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
 
+    #create lists to hold votes and candidates
     votes = []
     candidates = []
     
     skip_head = 1
 
+    #counts votes
     for row in csvreader:
         if skip_head:
             skip_head = 0
@@ -19,19 +26,23 @@ with open(os.path.abspath(election_data)) as csvfile:
             
     row_count = len(votes)
 
+    #finds unique candidate names
     for row in votes:
         candidate = row[2]
         if candidate not in candidates: 
             candidates.append(candidate) 
     
-    print('Election Results')
-    print("-------------------------")
-    print(f'Total Votes: {row_count}')
-    print("-------------------------")
+    #print statments
+    output.append('Election Results')
+    output.append("-------------------------")
+    output.append(f'Total Votes: {row_count}')
+    output.append("-------------------------")
 
+    #create variable to hold winner
     winner = ""
     max_votes = 0
 
+    #calculates votes per candidate and percentage of votes
     for candidate in candidates:
         total_votes = 0
         for row in votes:
@@ -41,8 +52,16 @@ with open(os.path.abspath(election_data)) as csvfile:
             max_votes = total_votes
             winner = candidate
         percentage = total_votes/row_count*100
-        print(f'{candidate}: {percentage:.3f}% ({total_votes})')
+        output.append(f'{candidate}: {percentage:.3f}% ({total_votes})')
 
-    print("-------------------------")
-    print(f'Winner: {winner}')
-    print("-------------------------")
+    #print statements
+    output.append("-------------------------")
+    output.append(f'Winner: {winner}')
+    output.append("-------------------------")
+
+    #output to text file
+    with open ("output.txt","w") as f:
+        f.writelines(output)
+    #output to terminal
+    for line in output: 
+        print(line)
